3adc57d1de1018ec413e43cd7bb95e553f49356c
Daniel Drazek <daniel_2000@o2.pl>
2022-06-22 20:59:08 +0100

    Configure DataBase by connection pool

--- src/main/java/pl/pp/manager/model/WorkersDBUtils.java
+++ src/main/java/pl/pp/manager/model/WorkersDBUtils.java
@@ -1,26 +1,17 @@
 package pl.pp.manager.model;
 
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class WorkersDBUtils {
-    public static List<Worker> getWorkers(){
+    public static List<Worker> getWorkers(DataSource dataSource){
         System.out.println("metoda getWorkers");
         List<Worker> workers = new ArrayList<>();
 
-        String url = "jdbc:mysql://127.0.0.1:3306/worker_manager";
-        String user = "root";
-        String password = "";
-
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
         //database connection
-        try (Connection connection = DriverManager.getConnection(url, user, password);
+        try (Connection connection = dataSource.getConnection();
         Statement statement = connection.createStatement()){    //make statement
 
             //get all workers from database
